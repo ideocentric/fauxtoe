11abd11149ddeb48db48c7d7b1e4112259827fe9
Matt Comeione <6080346+ideocentric@users.noreply.github.com>
2026-09-23 21:40:44 -0700

    Initial Commit

---
+++ fauxto/fauxtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  fauxtoApp.swift
+//  fauxto
+//
+//  Created by Matt Comeione on 9/23/26.
+//
+
+import SwiftUI
+
+@main
+struct fauxtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fauxtoTests/fauxtoTests.swift
@@ -0,0 +1,17 @@
+//
+//  fauxtoTests.swift
+//  fauxtoTests
+//
+//  Created by Matt Comeione on 9/23/26.
+//
+
+import Testing
+@testable import fauxto
+
+struct fauxtoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
